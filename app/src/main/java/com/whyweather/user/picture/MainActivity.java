84b02d419e43c1606c9a5ab6128f4b684ade783b
JongHo.kim <pia-a@hanmail.net>
2017-04-03 21:08:39 +0900

    ● 수정사항 - 버튼 누를 시 현 위치로 마커 이동

--- app/src/main/java/com/whyweather/user/picture/MainActivity.java
+++ app/src/main/java/com/whyweather/user/picture/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
@@ -147,6 +148,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        }
 
         mMap.setOnMapLongClickListener(this);
         if (mWeatherData == null) {
